6eafe857d83b671158c88ea5556ead4a3881f1b9
Ben Heasly <benjamin.heasly@gmail.com>
2013-04-17 14:02:17 -0400

    RGBPromotion makes a figure.

--- ExampleScenes/RGBPromotion/RGBPromotion.m
+++ ExampleScenes/RGBPromotion/RGBPromotion.m
@@ -20,8 +20,8 @@ spd = GenerateCIEDay(temp, B_cieday);
 wls = SToWls(S_cieday);
 blueDay = WriteSpectrumFile(wls, spd, sprintf('CIE-daylight-%d.spd', temp));
 
-illuminants = {'300:1 800:1', 'D65.spd', yellowDay, blueDay};
-RGBs = {[.9 0 0], [0 .9 0], [0 0 .9], [.5 .5 .5]};
+illuminants = {yellowDay, blueDay};
+RGBs = {[.8 .1 .4], [.5 .5 .5]};
 
 %% Get RGB promotions from PBRT and Mitsuba
 renderers = {'Mitsuba', 'PBRT'};
@@ -29,19 +29,113 @@ nRenderers = numel(renderers);
 nIlluminants = numel(illuminants);
 nRGBs = numel(RGBs);
 promotions = cell(nRenderers, nIlluminants, nRGBs);
-Ss = cell(nRenderers, nIlluminants, nRGBs);
+SOuts = cell(nRenderers, nIlluminants, nRGBs);
 RGBOuts = cell(nRenderers, nIlluminants, nRGBs);
 dataFiles = cell(nRenderers, nIlluminants, nRGBs);
 for rend = 1:nRenderers
-    for ilum = 1:nIlluminants
+    for illum = 1:nIlluminants
         for rgb = 1:nRGBs
             hints.renderer = renderers{rend};
             [promoted, S, RGB, dataFile] = ...
-                PromoteRGBReflectance(RGBs{rgb}, illuminants{ilum}, hints);
-            promotions{rend, ilum, rgb} = promoted;
-            Ss{rend, ilum, rgb} = S;
-            RGBOuts{rend, ilum, rgb} = RGB;
-            dataFiles{rend, ilum, rgb} = dataFile;
+                PromoteRGBReflectance(RGBs{rgb}, illuminants{illum}, hints);
+            promotions{rend, illum, rgb} = promoted;
+            SOuts{rend, illum, rgb} = S;
+            RGBOuts{rend, illum, rgb} = RGB;
+            dataFiles{rend, illum, rgb} = dataFile;
         end
     end
 end
+
+%% Plot RGB, and promoted spectra.
+close all
+hints = GetDefaultHints();
+RGBMarkers = {'x', 'o'};
+RGBOutMarkers = {'+', 'square'};
+spectrumMarkers = {'x', 'o'};
+RGBLegend = {};
+if hints.isPlot
+    fig = figure();
+    nCols = nRGBs * 2;
+    nRows = nIlluminants;
+    for illum = 1:nIlluminants
+        for rgb = 1:nRGBs
+            % for plotting RGB values
+            row = illum;
+            sp = (2*rgb-1) + (illum-1)*nCols;
+            maxPower = max(RGBs{rgb});
+            axRGB = subplot(nRows, nCols, sp, ...
+                'Parent', fig, ...
+                'YLim', [0 1.1*maxPower], ...
+                'YTick', [0 maxPower], ...
+                'YTickLabel', {}, ...
+                'XDir', 'reverse', ...
+                'XLim', [.9 3.1], ...
+                'XTick', 1:3, ...
+                'XTickLabel', {});
+            
+            % for plotting promoted spectra
+            outWls = MakeItWls(SOuts{1, illum, rgb});
+            axSpectra = subplot(nRows, nCols, sp+1, ...
+                'Parent', fig, ...
+                'YLim', [0 1.1*maxPower], ...
+                'YTick', [0 maxPower], ...
+                'YTickLabel', {}, ...
+                'XLim', [min(outWls) max(outWls)] + [-.1 .1], ...
+                'XTick', [min(outWls) max(outWls)], ...
+                'XTickLabel', {});
+            
+            % label outside plots
+            if 1 == illum
+                rgbName = sprintf('BGR=[%0.1f %0.1f %0.1f]', ...
+                    RGBs{rgb}(3), RGBs{rgb}(2), RGBs{rgb}(1));
+                title(axRGB, rgbName);
+                title(axSpectra, 'promoted');
+            end
+            
+            if 1 == rgb
+                [illumPath, illumName] = fileparts(illuminants{illum});
+                ylabel(axRGB, illumName);
+                set(axRGB, 'YTickLabel', {'0', 'max power'});
+            end
+            
+            if illum == nIlluminants
+                set(axRGB, 'XTickLabel', {'R', 'G', 'B'});
+                set(axSpectra, 'XTickLabel', [min(outWls) max(outWls)])
+                xlabel(axSpectra, 'nm')
+            end
+            
+            for rend = 1:nRenderers
+                % plot original RGB
+                plotColor = RGBs{rgb};
+                line(1:3, RGBs{rgb}, ...
+                    'Parent', axRGB, ...
+                    'Color', plotColor, ...
+                    'Marker', RGBMarkers{rend}, ...
+                    'LineStyle', 'none');
+                
+                % plot recovered RGB
+                line(1:3, RGBOuts{rend, illum, rgb}, ...
+                    'Parent', axRGB, ...
+                    'Color', plotColor, ...
+                    'Marker', RGBOutMarkers{rend}, ...
+                    'LineStyle', 'none');
+                
+                % plot promoted spectra
+                line(outWls, promotions{rend, illum, rgb}, ...
+                    'Parent', axSpectra, ...
+                    'Color', plotColor, ...
+                    'Marker', spectrumMarkers{rend}, ...
+                    'LineStyle', 'none');
+                
+                % remember RGB legend info
+                rgbIndex = (rend-1)*nRenderers;
+                RGBLegend{rgbIndex+1} = sprintf('in %s', renderers{rend});
+                RGBLegend{rgbIndex+2} = sprintf('out %s', renderers{rend});
+            end
+        end
+    end
+end
+
+% put a legend on the last axes
+legend(axRGB, RGBLegend, 'Location', 'southwest');
+legend(axSpectra, renderers, 'Location', 'southwest');
