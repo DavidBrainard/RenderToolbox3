0920fe32b6ad24797c32c1ee27f2c820db2b35c0
Ben Heasly <benjamin.heasly@gmail.com>
2013-04-17 15:27:48 -0400

    Starting new "Bumps" bump map variation of MaterialSphere scene.

---
+++ ExampleScenes/MaterialSphere/MakeMaterialSphereBumps.m
@@ -0,0 +1,47 @@
+%%% RenderToolbox3 Copyright (c) 2012 The RenderToolbox3 Team.
+%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
+%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
+%
+%% Render the MaterialSphere scene with 3 materials and a bump map.
+clear;
+clc;
+
+%% Choose example files, make sure they're on the Matlab path.
+AddWorkingPath(mfilename('fullpath'));
+sceneFile = 'MaterialSphere.dae';
+conditionsFile = 'MaterialSphereConditions.txt';
+mappingsFile = 'MaterialSphereBumpsMappings.txt';
+
+%% Choose batch renderer options.
+% which materials to use, [] means all
+hints.whichConditions = [];
+
+% pixel size of each rendering
+hints.imageWidth = 200;
+hints.imageHeight = 160;
+
+%% Render with Mitsuba and PBRT.
+
+% how to convert multi-spectral images to sRGB
+toneMapFactor = 100;
+isScaleGamma = true;
+
+% make a montage and sensor images with each renderer
+for renderer = {'Mitsuba', 'PBRT'}
+    
+    % choose one renderer
+    hints.renderer = renderer{1};
+    
+    % make 3 multi-spectral renderings, saved in .mat files
+    sceneFiles = MakeSceneFiles(sceneFile, conditionsFile, mappingsFile, hints);
+    outFiles = BatchRender(sceneFiles, hints);
+    
+    % condense multi-spectral renderings into one sRGB montage
+    montageName = sprintf('MaterialSphereBumps (%s)', hints.renderer);
+    montageFile = [montageName '.png'];
+    [SRGBMontage, XYZMontage] = ...
+        MakeMontage(outFiles, montageFile, toneMapFactor, isScaleGamma, hints);
+    
+    % display the sRGB montage
+    ShowXYZAndSRGB([], SRGBMontage, montageName);
+end
