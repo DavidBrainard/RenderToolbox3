b06768f1af9f306d72e3746868bda1a09ea8b79f
Ben Heasly <benjamin.heasly@gmail.com>
2013-06-14 13:56:42 -0400

    MakeInterior restore missing spectrum file creation.

--- ExampleScenes/Interior/MakeInterior.m
+++ ExampleScenes/Interior/MakeInterior.m
@@ -15,6 +15,35 @@ mappingsFile = 'InteriorMappings.txt';
 hints.whichConditions = [];
 hints.outputSubfolder = mfilename();
 
+%% Move to temp folder before creating new files.
+originalFolder = pwd();
+tempFolder = GetOutputPath('tempFolder', hints);
+AddWorkingPath(tempFolder);
+cd(tempFolder);
+
+%% Write some spectra to use.
+load B_cieday
+
+% make orange-yellow for a few lights
+temp = 4000;
+scale = 3;
+spd = scale * GenerateCIEDay(temp, B_cieday);
+wls = SToWls(S_cieday);
+WriteSpectrumFile(wls, spd, sprintf('YellowLight.spd', temp));
+
+% make strong yellow for the hanging spot light
+temp = 5000;
+scale = 30;
+spd = scale * GenerateCIEDay(temp, B_cieday);
+wls = SToWls(S_cieday);
+WriteSpectrumFile(wls, spd, sprintf('HangingLight.spd', temp));
+
+% make daylight for the windows behind the camera
+[wavelengths, magnitudes] = ReadSpectrum('D65.spd');
+scale = 1;
+magnitudes = scale * magnitudes;
+WriteSpectrumFile(wavelengths, magnitudes, 'WindowLight.spd');
+
 %% Render with Mitsuba and PBRT
 toneMapFactor = 10;
 isScale = true;
@@ -23,7 +52,7 @@ for renderer = {'Mitsuba', 'PBRT'}
     sceneFiles = MakeSceneFiles(sceneFile, conditionsFile, mappingsFile, hints);
     outFiles = BatchRender(sceneFiles, hints);
     
-    % write each condition to a separte image file
+    % write each condition to a separate image file
     for ii = 1:numel(outFiles)
         [outPath, outBase, outExt] = fileparts(outFiles{ii});
         montageName = sprintf('%s (%s)', outBase, hints.renderer);
