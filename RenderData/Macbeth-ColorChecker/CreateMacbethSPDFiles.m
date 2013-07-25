3927cb877ec85fb88c449a5001009570e6960795
Ben Heasly <benjamin.heasly@gmail.com>
2013-07-25 18:10:05 -0400

    Update RenderData/ doxygen comments for version 1.0.

--- RenderData/Macbeth-ColorChecker/CreateMacbethSPDFiles.m
+++ RenderData/Macbeth-ColorChecker/CreateMacbethSPDFiles.m
@@ -2,9 +2,10 @@
 %%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
 %%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
 %
-% Convert Macbeth ColorChecker colorimetric data from the Psychtoolbox .mat
-% file format to the .spd format expected by PBRT and Mitsuba.
-%
+%% Import Macbeth ColorChecker colorimetric from Psychtoolbox format. 
+
+% convert Psychtoolbox colorimetric mat-files to spd-files expected by PBRT
+% and Mitsuba
 [macbethPath, macbethName] = fileparts(mfilename('fullpath'));
 matFile = fullfile(macbethPath, 'sur_mccBabel.mat');
 outFiles = ImportPsychColorimetricMatFile(matFile);
