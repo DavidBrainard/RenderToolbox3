3d33bcb2e1421024f0d6d70b46fdfab5a7a2d85c
Ben Heasly <benjamin.heasly@gmail.com>
2013-07-26 18:34:48 -0400

    Shell script to help make repository tags for RenderToolbox3 releases.

---
+++ Utilities/create-release-tags.sh
@@ -0,0 +1,68 @@
+# This script creates git tags for RenderToolbox3 releases and pushes them to GitHub.
+#
+# It will only work if you have permission to push to the RenderToolbox3 repositories.
+#
+# If you are making a RenderToolbox3 release, you should copy this script and edit it
+# with values like your local  doxygen path, and the version RenderToolbox3 that you
+# are releasing.
+#
+# If nothing else, this script should document how to do a RenderToolbox3 release.
+#
+
+# edit these to match your local system
+RTB_VERSION="v1.0"
+RTB_REPO="git@github.com:DavidBrainard/RenderToolbox3.git"
+RTB_WIKI="git@github.com:DavidBrainard/RenderToolbox3.wiki.git"
+DOXYGEN="/Applications/Doxygen.app/Contents/Resources/doxygen" 
+WORKING_FOLDER="/Users/ben/RenderToolbox3-release-temp"
+
+mkdir $WORKING_FOLDER
+
+### make a tag for the RenderToolbox3 master branch
+echo
+echo "Tagging RenderToolbox3 $RTB_VERSION"
+echo
+
+cd $WORKING_FOLDER
+git clone $RTB_REPO
+cd RenderToolbox3
+git checkout master
+git tag -a $RTB_VERSION -m "tag for RenderToolbox3 version $RTB_VERSION"
+git push origin --tags
+
+### generate fresh doxygen documentation based on master branch, stored in gh-pages branch
+echo
+echo "Regenerating Doxygen docs"
+echo
+
+cd $WORKING_FOLDER
+cp -r RenderToolbox3 RenderToolbox3-gh-pages
+cd RenderToolbox3-gh-pages
+git checkout gh-pages
+cp RenderToolbox3Doxyfile RenderToolbox3Doxyfile-temp
+echo "PROJECT_NUMBER=$RTB_VERSION" >> RenderToolbox3Doxyfile-temp
+rm -r docs
+$DOXYGEN RenderToolbox3Doxyfile-temp
+rm RenderToolbox3Doxyfile-temp
+
+### make a tag for the new docs in the gh-pages branch
+echo
+echo "Tagging RenderToolbox3 $RTB_VERSION-docs"
+echo
+
+cd $WORKING_FOLDER/RenderToolbox3-gh-pages
+git add .
+git commit -m "fresh docs for RenderToolbox3 version $RTB_VERSION"
+git tag -a $RTB_VERSION-docs -m "tag for RenderToolbox3 version $RTB_VERSION-docs"
+git push origin --tags
+
+### make a tag for the RenderToolbox3 wiki
+echo
+echo "Tagging RenderToolbox3 $RTB_VERSION-wiki"
+echo
+
+cd $WORKING_FOLDER
+git clone $RTB_WIKI RenderToolbox3-wiki
+cd RenderToolbox3-wiki
+git tag -a $RTB_VERSION-wiki -m "tag for RenderToolbox3 version $RTB_VERSION-wiki"
+git push origin --tags
