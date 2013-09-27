790dab8b2a657dfc58df3e05e46ddd3884bd768c
Ben Heasly <benjamin.heasly@gmail.com>
2013-09-27 16:20:51 -0400

    Renderer plugins should put all scene data and metadata in one scene variable.

--- RendererPlugins/SampleRenderer/RTB_DataToRadiance_SampleRenderer.m
+++ RendererPlugins/SampleRenderer/RTB_DataToRadiance_SampleRenderer.m
@@ -4,7 +4,7 @@
 %
 % Convert Sample Renderer data to units of radiance.
 %   @param multispectralImage numeric rendering data from a Render function
-%   @param sceneData data about the rendered scene to aid conversion
+%   @param scene description of the scene from an ImportCollada function
 %   @param hints struct of RenderToolbox3 options
 %
 % @details
@@ -22,11 +22,12 @@
 % @details
 % A DataToRadiance function must convert the @a multispectralImage data
 % that was returned from a RenderToolbox3 Render function into inits of
-% physical radiance.  It must also accept a @a sceneData parameter, as
-% returned from a RenderToolbox3 ImportCollada function, which may provide
-% scene- and renderer-specific data that informs the conversion to radiance
-% units.  It must also accept a @a hints parameter of RenderTooblox3
-% options, which also may inform the conversion.
+% physical radiance.  It must also accept a @a scne parameter, as
+% returned from a RenderToolbox3 ImportCollada function, which describes
+% the rendered scene and may provide scene-specific and renderer-specific
+% data to informs the conversion to radiance units.  It must also accept a
+% @a hints parameter of RenderTooblox3 options, which also may inform the
+% conversion.
 %
 % @details
 % Converting a multispectralImage to units of physical radiance units is a
@@ -41,28 +42,28 @@
 % @details
 % For some renderers, the proper scale factor may depend on partcular scene
 % parameters, like the nubmer of ray samples used or the type of sample
-% integrator.  In this case, @a sceneData and @a hints should be used to
-% choose or calculate a proper scale factor.  Getting this right may
-% require a lot of prior investigation, which may not be feasible in all
-% cases.  A DataToRadiance should make a best effort to return accurate
-% radiance units, and print a warning when it is unable to do so.
+% integrator.  In this case, @a scene and @a hints should be used to choose
+% or calculate a proper scale factor.  Getting this right may require a lot
+% of prior investigation, which may not be feasible in all cases.  A
+% DataToRadiance should make a best effort to return accurate radiance
+% units, and raise a warning when it is unable to do so.
 %
 % @details
 % A DataToRadiance function must return the given @a multispectralImage,
 % scaled into units of physical radiance.  It must also return the scale
-% factor that it applied to the original @a multispectralImage.
+% factor that was applied to the original @a multispectralImage.
 %
 % @details
 % Usage:
-%   [radianceData, scaleFactor] = RTB_DataToRadiance_SampleRenderer(multispectralImage, sceneData, hints)
+%   [radianceImage, scaleFactor] = RTB_DataToRadiance_SampleRenderer(multispectralImage, scene, hints)
 %
 % @ingroup RendererPlugins
-function [radianceImage, scaleFactor] = RTB_DataToRadiance_SampleRenderer(multispectralImage, sceneData, hints)
+function [radianceImage, scaleFactor] = RTB_DataToRadiance_SampleRenderer(multispectralImage, scene, hints)
 
 disp('SampleRenderer DataToRadiance function')
 disp('multispectralImage is:')
 disp(multispectralImage);
-disp('sceneData is:')
+disp('scene is:')
 disp(sceneData);
 disp('hints is:')
 disp(hints);
--- RendererPlugins/SampleRenderer/RTB_ImportCollada_SampleRenderer.m
+++ RendererPlugins/SampleRenderer/RTB_ImportCollada_SampleRenderer.m
@@ -13,9 +13,9 @@
 % function.
 %
 % @details
-% The name of an ImportCollada function must match a specific pattern: it must
-% begin with "RTB_ImportCollada_", and it must end with the name of the
-% renderer, for example, "SampleRenderer".  This pattern allows
+% The name of an ImportCollada function must match a specific pattern: it
+% must begin with "RTB_ImportCollada_", and it must end with the name of
+% the renderer, for example, "SampleRenderer".  This pattern allows
 % RenderToolbox3 to automatically locate the ImportCollada function for
 % each renderer.  ImportCollada functions should be included in the Matlab
 % path.
@@ -24,12 +24,12 @@
 % An ImportCollada function must convert the given @a colladaFile ('.dae'
 % or '.xml') to a native format that the renderer can use for rendering.
 % It may write a new native scene file at the specified @a outFile.  It may
-% also build up a representation of the native scene to return directly as
-% a Matlab variable.
+% also create a representation of the native scene in Matlab and return the
+% representation directly.
 %
 % @details
 % An ImportCollada function may use the given @a adjustments to modify
-% the native scene files following the initial conversion from Collada.  @a
+% the native scene, following an initial conversion from Collada.  @a
 % adjustments may contain scene data or the name of a partial scene file to
 % be merged with the scene in a renderer-specific way.  @a adjustments will
 % be returned from a RenderToolbox3 ApplyMappings function.
@@ -40,18 +40,17 @@
 %
 % @details
 % An ImportCollada function must return a scene description in
-% renderer-native format.  This may be the name of the @a new outFile, or a
-% Matlab variable that contains the scene description.  It must also return
-% a variable containing meta-data about the scene.  This variable may be
-% empty, or it may contain additional data such as an XML representation of
-% the scece, or other meta-data.
+% renderer-native format using a single matlab variable, such as a string,
+% struct or object.  This description should include the name of the @a new 
+% outFile, any other new files that were created, and any other
+% representation of the scene.
 %
 % @details
 % Usage:
-%   [scene, sceneData] = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
+%   scene = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
 %
 % @ingroup RendererPlugins
-function [scene, sceneData] = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
+function scene = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
 
 disp('SampleRenderer ImportCollada function.');
 disp('colladaFile is:');
@@ -63,8 +62,7 @@ disp(adjustments);
 disp('hints is:');
 disp(hints);
 
+scene.description = 'SampleRenderer scene description';
 scene.height = 5;
 scene.width = 5;
 scene.value = 1;
-
-sceneData = 'SampleRenderer scene data';
