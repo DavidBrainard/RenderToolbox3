b848bbcc630bf454d00dfeac19e4abd1c7b29973
Ben Heasly <benjamin.heasly@gmail.com>
2013-09-28 14:48:52 -0400

    Continuing plugin implementations for PBRT and Mitsuba.

--- RendererPlugins/Mitsuba/RTB_ImportCollada_Mitsuba.m
+++ RendererPlugins/Mitsuba/RTB_ImportCollada_Mitsuba.m
@@ -2,67 +2,79 @@
 %%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
 %%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
 %
-% Convert a Collada parent scene file the Sample Renderer native format
+% Convert a Collada parent scene file the Mitsuba native format
 %   @param colladaFile input Collada parent scene file name or path
-%   @param outFile output native scene file name or path
 %   @param adjustments native adjustments data, or file name or path
+%   @param outputFolder folder where to write new files
+%   @param imageName the name to use for this scene and new files
 %   @param hints struct of RenderToolbox3 options
 %
 % @details
-% This function is a template for a RenderToolbox3 "ImportCollada"
-% function.
+% This is the RenderToolbox3 "ImportCollada" function for Mitsuba.
 %
 % @details
-% The name of an ImportCollada function must match a specific pattern: it
-% must begin with "RTB_ImportCollada_", and it must end with the name of
-% the renderer, for example, "SampleRenderer".  This pattern allows
-% RenderToolbox3 to automatically locate the ImportCollada function for
-% each renderer.  ImportCollada functions should be included in the Matlab
-% path.
-%
-% @details
-% An ImportCollada function must convert the given @a colladaFile ('.dae'
-% or '.xml') to a native format that the renderer can use for rendering.
-% It may write a new native scene file at the specified @a outFile.  It may
-% also create a representation of the native scene in Matlab and return the
-% representation directly.
-%
-% @details
-% An ImportCollada function may use the given @a adjustments to modify
-% the native scene, following an initial conversion from Collada.  @a
-% adjustments may contain scene data or the name of a partial scene file to
-% be merged with the scene in a renderer-specific way.  @a adjustments will
-% be returned from a RenderToolbox3 ApplyMappings function.
-%
-% @details
-% @a hints will be a struct with RenderToolbox3 options, as returned from
-% GetDefaultHints(), which may inform the conversion process.
-%
-% @details
-% An ImportCollada function must return a scene description in
-% renderer-native format using a single matlab variable, such as a string,
-% struct or object.  This description should include the name of the @a new 
-% outFile, any other new files that were created, and any other
-% representation of the scene.
+% For more about ImportCollada functions see
+% RTB_ImportCollada_SampleRenderer().
 %
 % @details
 % Usage:
-%   scene = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
+%   [scene, requiredFiles] = RTB_ImportCollada_Mitsuba(colladaFile, adjustments, outputFolder, imageName, hints)
 %
 % @ingroup RendererPlugins
-function scene = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
+function [scene, requiredFiles] = RTB_ImportCollada_Mitsuba(colladaFile, adjustments, outputFolder, imageName, hints)
 
-disp('SampleRenderer ImportCollada function.');
-disp('colladaFile is:');
-disp(colladaFile);
-disp('outFile is:');
-disp(outFile);
-disp('adjustments is:');
-disp(adjustments);
-disp('hints is:');
-disp(hints);
+% declare new and required files
+scene.colladaFile = colladaFile;
+scene.mitsubaFile = fullfile(outputFolder, [imageName '.xml']);
+scene.unadjustedMitsubaFile = ...
+    fullfile(outputFolder, [imageName 'Unadjusted.xml']);
+scene.adjustments = adjustments;
+requiredFiles = {scene.colladaFile, scene.pbrtFile, ...
+    scene.unadjustedMitsubaFile, scene.adjustments};
 
-scene.description = 'SampleRenderer scene description';
-scene.height = 5;
-scene.width = 5;
-scene.value = 1;
+% high-dynamic-range is a good default film for Mitsuba
+if isempty(hints.filmType)
+    hints.filmType = 'hdrfilm';
+end
+
+%% Invoke the Mitsuba importer.
+% set the dynamic library search path
+[newLibPath, originalLibPath, libPathName] = SetRenderToolboxLibraryPath();
+
+% find the Mitsuba importer
+%   don't pass the adjustments file to the converter
+[colladaPath, colladaBase, colladaExt] = fileparts(colladaFile);
+importer = fullfile( ...
+    getpref('Mitsuba', 'app'), ...
+    getpref('Mitsuba', 'importer'));
+fprintf('Converting %s\n  to %s.\n', colladaFile, scene.mitsubaFile);
+importCommand = sprintf('%s -r %dx%d -l %s %s %s', ...
+    importer, ...
+    hints.imageWidth, hints.imageHeight, ...
+    filmType, ...
+    [colladaBase colladaExt], scene.unadjustedMitsubaFile);
+
+% run in the destination folder to capture all ouput there
+originalFolder = pwd();
+cd(outputFolder);
+[status, result] = unix(importCommand);
+cd(originalFolder)
+if status ~= 0
+    error('Mitsuba file conversion failed\n  %s\n  %s\n', ...
+        colladaFile, result);
+end
+
+% restore the library search path
+setenv(libPathName, originalLibPath);
+
+%% Apply adjustments file using the RenderToolbox3 custom mechanism.
+%   Mitsuba nodes named "ref" have "id" attrubutes, but are not "id" nodes
+excludePattern = '^ref$';
+mitsubaDoc = ReadSceneDOM(scene.unadjustedMitsubaFile, excludePattern);
+adjustmentsDoc = ReadSceneDOM(scene.adjustments, excludePattern);
+MergeAdjustments(mitsubaDoc, adjustmentsDoc, excludePattern);
+WriteSceneDOM(scene.mitsubaFile, mitsubaDoc);
+
+%% Detect auxiliary geometry files.
+auxiliaryFiles = FindFiles(mitsubaPath, '\.serialized');
+requiredFiles = cat(2, requiredFiles, auxiliaryFiles);
--- RendererPlugins/PBRT/RTB_ImportCollada_PBRT.m
+++ RendererPlugins/PBRT/RTB_ImportCollada_PBRT.m
@@ -2,67 +2,78 @@
 %%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
 %%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
 %
-% Convert a Collada parent scene file the Sample Renderer native format
+% Convert a Collada parent scene file the PBRT native format
 %   @param colladaFile input Collada parent scene file name or path
-%   @param outFile output native scene file name or path
 %   @param adjustments native adjustments data, or file name or path
+%   @param outputFolder folder where to write new files
+%   @param imageName the name to use for this scene and new files
 %   @param hints struct of RenderToolbox3 options
 %
 % @details
-% This function is a template for a RenderToolbox3 "ImportCollada"
-% function.
+% This is the RenderToolbox3 "ImportCollada" function for PBRT.
 %
 % @details
-% The name of an ImportCollada function must match a specific pattern: it
-% must begin with "RTB_ImportCollada_", and it must end with the name of
-% the renderer, for example, "SampleRenderer".  This pattern allows
-% RenderToolbox3 to automatically locate the ImportCollada function for
-% each renderer.  ImportCollada functions should be included in the Matlab
-% path.
-%
-% @details
-% An ImportCollada function must convert the given @a colladaFile ('.dae'
-% or '.xml') to a native format that the renderer can use for rendering.
-% It may write a new native scene file at the specified @a outFile.  It may
-% also create a representation of the native scene in Matlab and return the
-% representation directly.
-%
-% @details
-% An ImportCollada function may use the given @a adjustments to modify
-% the native scene, following an initial conversion from Collada.  @a
-% adjustments may contain scene data or the name of a partial scene file to
-% be merged with the scene in a renderer-specific way.  @a adjustments will
-% be returned from a RenderToolbox3 ApplyMappings function.
-%
-% @details
-% @a hints will be a struct with RenderToolbox3 options, as returned from
-% GetDefaultHints(), which may inform the conversion process.
-%
-% @details
-% An ImportCollada function must return a scene description in
-% renderer-native format using a single matlab variable, such as a string,
-% struct or object.  This description should include the name of the @a new 
-% outFile, any other new files that were created, and any other
-% representation of the scene.
+% For more about ImportCollada functions see
+% RTB_ImportCollada_SampleRenderer().
 %
 % @details
 % Usage:
-%   scene = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
+%   [scene, requiredFiles] = RTB_ImportCollada_PBRT(colladaFile, adjustments, outputFolder, imageName, hints)
 %
 % @ingroup RendererPlugins
-function scene = RTB_ImportCollada_SampleRenderer(colladaFile, outFile, adjustments, hints)
+function [scene, requiredFiles] = RTB_ImportCollada_PBRT(colladaFile, adjustments, outputFolder, imageName, hints)
 
-disp('SampleRenderer ImportCollada function.');
-disp('colladaFile is:');
-disp(colladaFile);
-disp('outFile is:');
-disp(outFile);
-disp('adjustments is:');
-disp(adjustments);
-disp('hints is:');
-disp(hints);
+% declare new and required files
+scene.colladaFile = colladaFile;
+scene.pbrtFile = fullfile(outputFolder, [imageName '.pbrt']);
+scene.pbrtXMLFile = fullfile(outputFolder, [imageName '.pbrt.xml']);
+scene.adjustments = adjustments;
+requiredFiles = {scene.colladaFile, scene.pbrtFile, ...
+    scene.pbrtXMLFile, scene.adjustments};
 
-scene.description = 'SampleRenderer scene description';
-scene.height = 5;
-scene.width = 5;
-scene.value = 1;
+% image is a safe default film for PBRT
+if isempty(hints.filmType)
+    hints.filmType = 'image';
+end
+
+%% Invoke several Collada to PBRT utilities.
+fprintf('Converting %s\n  to %s.\n', colladaFile, scene.pbrtFile);
+
+% run in the destination folder to capture all ouput there
+originalFolder = pwd();
+cd(outputFolder);
+
+% read the collada file
+[colladaDoc, colladaIDMap] = ReadSceneDOM(colladaFile);
+
+% create a new PBRT-XML document
+[pbrtDoc, pbrtIDMap] = CreateStubDOM(colladaIDMap, 'pbrt_xml');
+PopulateStubDOM(pbrtIDMap, colladaIDMap, hints);
+
+% add a film node to the to the adjustments document
+filmNodeID = 'film';
+filmPBRTIdentifier = 'Film';
+[adjustDoc, adjustIDMap] = ReadSceneDOM(scene.adjustments);
+adjustRoot = adjustDoc.getDocumentElement();
+filmNode = CreateElementChild(adjustRoot, filmPBRTIdentifier, filmNodeID);
+adjustIDMap(filmNodeID) = filmNode;
+
+% fill in the film parameters
+SetType(adjustIDMap, filmNodeID, filmPBRTIdentifier, hints.filmType);
+AddParameter(adjustIDMap, filmNodeID, ...
+    'xresolution', 'integer', hints.imageWidth);
+AddParameter(adjustIDMap, filmNodeID, ...
+    'yresolution', 'integer', hints.imageHeight);
+
+% write the adjusted PBRT-XML document to file
+MergeAdjustments(pbrtDoc, adjustDoc);
+WriteSceneDOM(scene.pbrtXMLFile, pbrtDoc);
+
+% dump the PBRT-XML document into a .pbrt text file
+WritePBRTFile(scene.pbrtFile, scene.pbrtXMLFile, hints);
+
+cd(originalFolder)
+
+%% Detect auxiliary geometry files.
+auxiliaryFiles = FindFiles(outputPath, 'mesh-data-[^\.]+.pbrt');
+requiredFiles = cat(2, requiredFiles, auxiliaryFiles);
