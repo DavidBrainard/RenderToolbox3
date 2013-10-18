60e1bdc51050753163f5ed0a53040c439110ffcf
Ben Heasly <benjamin.heasly@gmail.com>
2013-10-18 19:40:04 -0400

    fresh docs for RenderToolbox3 version v1.1

--- docs/html/search/all_63.js
+++ docs/html/search/all_63.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['cleanmatlabpath',['CleanMatlabPath',['../group___utilities.html#ga4ce5a5214a8d4dd1fd319f6fd9221c3f',1,'CleanMatlabPath.m']]],
   ['colladatomitsuba',['ColladaToMitsuba',['../group___utilities.html#ga7c0c95f63d05404d1caaa06da2d662b0',1,'ColladaToMitsuba.m']]],
-  ['colladatopbrt',['ColladaToPBRT',['../group___utilities.html#ga509d8833e7183548b2b4e36346b1b36b',1,'ColladaToPBRT(colladaFile, pbrtFile, adjustmentsFile, hints):&#160;ColladaToPBRT.m'],['../group___collada_to_p_b_r_t.html',1,'(Global Namespace)']]],
+  ['colladatopbrt',['ColladaToPBRT',['../group___utilities.html#ga509d8833e7183548b2b4e36346b1b36b',1,'ColladaToPBRT(colladaFile, pbrtFile, adjustmentsFile, hints):&#160;RTB_ImportCollada_SampleRenderer.m'],['../group___collada_to_p_b_r_t.html',1,'(Global Namespace)']]],
   ['computeradiometricscalefactors',['ComputeRadiometricScaleFactors',['../group___batch_renderer.html#ga549cc197fbfc7d88e081cfe1c70b7f77',1,'ComputeRadiometricScaleFactors.m']]],
   ['convertcamera',['ConvertCamera',['../group___collada_to_p_b_r_t.html#gaffe0a70ab60323321b1d735bc528c472',1,'ConvertCamera.m']]],
   ['convertgeometry',['ConvertGeometry',['../group___collada_to_p_b_r_t.html#gaaa17bcc18975d65d5d5684a43c4ca36f',1,'ConvertGeometry.m']]],
