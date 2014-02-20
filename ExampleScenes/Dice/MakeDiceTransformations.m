5f9163a64348804f601a23f5e17013f1c222fae7
Ben Heasly <benjamin.heasly@gmail.com>
2014-02-20 14:57:35 -0500

    DiceTransformations variant demonstrates dealing with 4x4 transformation matrices.

---
+++ ExampleScenes/Dice/MakeDiceTransformations.m
@@ -0,0 +1,34 @@
+%%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
+%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
+%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
+%
+%% Render the Dice scene with various spatial transformations.
+
+%% Choose example files, make sure they're on the Matlab path.
+AddWorkingPath(mfilename('fullpath'));
+parentSceneFile = 'Dice.dae';
+mappingsFile = 'DiceTransformationsMappings.txt';
+conditionsFile = 'DiceTransformationsConditions.txt';
+
+%% Choose batch renderer options.
+hints.imageWidth = 640;
+hints.imageHeight = 480;
+hints.outputSubfolder = mfilename();
+
+%% Render with Mitsuba and PBRT.
+toneMapFactor = 10;
+isScale = true;
+for renderer = {'Mitsuba'}%, 'PBRT'}
+    hints.renderer = renderer{1};
+    nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+    radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+    
+    for ii = 1:numel(radianceDataFiles)
+        [dataPath, imageName] = fileparts(radianceDataFiles{ii});
+        montageName = sprintf('Dice - %s (%s)', imageName, hints.renderer);
+        montageFile = [montageName '.png'];
+        [SRGBMontage, XYZMontage] = MakeMontage( ...
+            radianceDataFiles(ii), montageFile, toneMapFactor, isScale, hints);
+        ShowXYZAndSRGB([], SRGBMontage, montageName);
+    end
+end
