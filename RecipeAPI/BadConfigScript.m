ea82900478e6a587b40f6611d10af275ed9ee490
Ben Heasly <benjamin.heasly@gmail.com>
2014-05-16 14:05:26 -0400

    Remove unused testing scripts.

--- RecipeAPI/BadConfigScript.m
+++
@@ -1,9 +0,0 @@
-% this script should modify the current recipe and throw an error.
-
-run('RenderToolbox3ConfigurationTemplate');
-
-recipe = CurrentRecipe();
-recipe.input.hints.renderer = 'BadRenderer';
-CurrentRecipe(recipe);
-
-error('silly:id', 'This is a silly error');
